eeb4b078c37fa5b1ec15109c9484142b9ed4533c
aue-tripenko <67926726+san4ern@users.noreply.github.com>
2021-06-22 16:33:51 +0300

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -3,15 +3,15 @@ module.exports = {
     description: 'Average bot ping.',
     aliases: [ 'stats', 'ping', 'bs' ],
     cooldown: 1,
-    async execute(message, args) {
-        function average(nums) {
-            return nums.reduce((a, b) => (a + b)) / nums.length;
-        }
-        let reply = `🔄 **Websocket**: ${message.client.ws.ping}ms\n🔄 **Average Websocket**: ${average(message.client.ping)}ms\n🔄 **API**: ${m.createdTimestamp - message.createdTimestamp}ms\n` 
-        if (message.author.id == '607148903833403422') {
-            reply += `⚙️ **CPU usage**: ${average(require('os').loadavg())}\n⚙️ **RAM**: ${(process.memoryUsage().heapUsed / 1024 ** 2).toFixed(0)}`
-        }
-        const m = await message.channel.send('Pinging...')
-        m.edit(reply)
+    async execute(message, args) {function average(nums) {
+        return nums.reduce((a, b) => (a + b)) / nums.length;
+    }
+    
+    const m = await message.channel.send('Pinging...')
+    let reply = `🔄 **Websocket**: ${message.client.ws.ping}ms\n🔄 **Average Websocket**: ${average(message.client.ping)}ms\n🔄 **API**: ${m.createdTimestamp - message.createdTimestamp}ms\n` 
+    if (message.author.id == '607148903833403422') {
+        reply += `⚙️ **CPU usage**: ${average(require('os').loadavg())}\n⚙️ **RAM**: ${(process.memoryUsage().heapUsed / 1024 ** 2).toFixed(0)}`
+    }
+    m.edit(reply)
     }
 }
